b16bcd8d602f1f63ad82a516d67bc235609c48b2
Rodrigo D’Agostino <rodrigo.dagostino@gmail.com>
2021-08-05 18:39:08 -0300

    Add credits and their styles

--- src/layouts/TheFooter.vue
+++ src/layouts/TheFooter.vue
@@ -1,7 +1,9 @@
 <template>
 	<footer class="site-footer">
 		<div class="container">
-			
+			<a href="https://github.com/rodrigodagostino/vue-movie-search-app" target="_blank">
+				Made with <img src="@/assets/vue-logo.png" alt="Vue.js logo" /> by Rodrigo D’Agostino
+			</a>
 		</div>
 	</footer>
 </template>
@@ -9,7 +11,29 @@
 <style>
 .site-footer {
 	background-color: var(--gray-950);
+	font-size: 0.875rem;
+	text-align: center;
+	padding: 0.5rem 0;
 	margin-top: auto;
+}
+
+.site-footer a {
+	color: var(--gray-300);
+	text-decoration: none;
+	transition: color 0.24s ease;
+}
+
+.site-footer a:focus,
+.site-footer a:hover {
+	color: var(--white);
+}
+
+.site-footer img {
+	height: 1.25rem;
+	width: auto;
+	vertical-align: middle;
+	margin: 0 0.25rem;
+	display: inline-block;
 }
 
 .site-footer .container {
