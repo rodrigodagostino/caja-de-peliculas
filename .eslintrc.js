c3c6f95a93a3c38141895c703e087378720264e5
Rodrigo D’Agostino <rodrigo.dagostino@gmail.com>
2021-09-18 18:18:59 -0300

    Update packages, remove unnecessary ones, update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:vue/vue3-essential"],
   parserOptions: {
     ecmaVersion: 2018,
   },
